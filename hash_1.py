3edada50aee4efcb636be687bba7ef13fdbb27eb
John Lester Escarlan <jlescarlan11@gmail.com>
2024-01-28 18:55:41 +0800

    nothing necessary

---
+++ hash_1.py
@@ -0,0 +1,6 @@
+glossary = {'BDFL': 'Benevolent Dictator for Life'}
+glossary['GIL'] = 'Global Interpreter Lock' #   Add
+glossary['BDFL'] = 'Guido van Rossum'   #   Update
+del glossary['GIL'] #   Delete
+print(glossary['BDFL'])    #   Search
+print(glossary)
