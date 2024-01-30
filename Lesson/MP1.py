9938d2827ae2e23c58b3a87789fde19817d3af33
John Lester Escarlan <jlescarlan11@gmail.com>
2024-01-30 14:48:06 +0800

    dkj

---
+++ Lesson/MP1.py
@@ -0,0 +1,17 @@
+
+def average():
+    pass
+
+
+
+n = int(input())
+
+x_1 = [map(float, input('Enter a number ').split())]
+    # y_1 = list(map(float, input().split()))
+    # x_2 = list(map(float, input().split()))
+    # y_2 = list(map(float, input().split()))
+    # x_3 = list(map(float, input().split()))
+    # y_3 = list(map(float, input().split()))
+    # x_4 = list(map(float, input().split()))
+    # y_4 = list(map(float, input().split()))
+print(x_1)
